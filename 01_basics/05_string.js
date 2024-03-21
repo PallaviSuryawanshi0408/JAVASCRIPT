9d81843c35ea03b988e9c7ba9f6819e5c8a4c160
PallaviSuryawanshi0408 <128134690+PallaviSuryawanshi0408@users.noreply.github.com>
2024-03-21 11:15:40 +0000

    all about strings and different prototype uses

---
+++ 01_basics/05_string.js
@@ -0,0 +1,16 @@
+let Name = "Pallavi"
+let Age = 23
+// concatination of string
+console.log("my name is " + Name +" and my age is" + Age)
+//anothr and modern way string interpolition
+console.log(`my name is ${Name}`)
+
+const gameName = new String('ma-ri-o')
+
+ console.log(gameName[0]);
+ console.log(gameName.__proto__); 
+ console.log(gameName.length)
+ console.log(gameName.toUpperCase())
+//  with using direct variablename.prototype we can access any prototype use browser console to know more about different string prototype
+/******different prototype of string refer mdn documentation to learn more about these methods */
+// anchor,at,big,length,blink,bold,charAt,charCodeAt,codePointAt,concat,constructor,endsWith,fixed,fontcolor,fontsize,includes,indexOf,isWellFormed,italics,lastIndexOf,length,link,localeCompare,match,matchAll,normalize,padEnd,padStart,repeat,replace,replaceAll,search,slice,small,split,startsWith,strike,sub,substr,substring,sup,toLocaleLowerCase,toLocaleUpperCase,toLowerCase,toString,toUpperCase,toWellFormed,trim,trimEnd,trimLeft,trimRight,trimStart,valueOf
