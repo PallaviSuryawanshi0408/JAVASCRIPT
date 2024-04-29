81c2dddb2cdba710f8e32571327c44d1600e4205
PallaviSuryawanshi0408 <128134690+PallaviSuryawanshi0408@users.noreply.github.com>
2024-04-29 05:30:44 +0000

    arrow function

--- 03_basics/05_arrowfunction.js
+++ 03_basics/05_arrowfunction.js
@@ -13,3 +13,31 @@ user.username ="sam"
 user.welcome()
 
 console.log(this)//this will refer a empty object because we are in node environment globally it is not referring to any object but in browser this will refer some window object
+
+//through arrow function
+const newfunc = ()=>{
+let newname ="pallavi"
+console.log(this.newname); //undefined
+console.log(this);
+}
+newfunc()
+
+//arrow functon parameters
+
+// let arrowfunct = (num1,num2)=>{
+//     return num1+num2
+// }
+// console.log(arrowfunct(3,4))
+// or 
+let arrowfunct = (num1,num2)=>(num1+num2)
+console.log(arrowfunct(3,4));
+
+const addTwo = (num1, num2) => ({username: "hitesh"})
+
+
+console.log(addTwo(3, 4))
+
+
+// const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
