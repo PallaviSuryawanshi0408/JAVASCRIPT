cf693116a42d4accbd25a39d6effdd0c87faeada
PallaviSuryawanshi0408 <128134690+PallaviSuryawanshi0408@users.noreply.github.com>
2024-09-26 17:29:55 +0530

    Update 01_arrays.js

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -37,14 +37,32 @@ console.log( newArr);
 console.log(typeof newArr);
 
 //slice and splice method
-console.log("A ", MyArr);
+/*slice():
+The slice() method in JavaScript extracts a section of an array and returns a new array containing the selected elements, without modifying the original array.
+Syntax:
+array_name.slice(s, e)
+Example: In this example The slice() method in JavaScript extracts elements from the cars array, creating a new array new_cars from index 1 up to, but not including, index 4. */
 
-const myn1 = MyArr.slice(1, 3)
-console.log( myn1);
-console.log("B ", MyArr);
+
+let cars = ['Benz', 'Innova', 'Breeza', 'Etios', 'Dzire'];
+let new_cars = cars.slice(1, 4);
+console.log("cars :", cars);
+console.log("new_cars :", new_cars);
+
+/*splice():
+The splice() method in JavaScript is used to change the contents of an array by removing or replacing existing elements and/or adding new elements in place, modifying the original array.
+
+Syntax:
+array_name.splice(i, n, item 1, item 2, .....item n)
+Example: In this example, The splice() method in JavaScript inserts new elements (‘ambassador’, ‘BMW’, ‘Audi’) into the cars array at index 2 without removing any elements.*/
 
 
-const myn2 = MyArr.splice(1, 3)
-console.log("C ", MyArr);
-console.log(myn2);
+let cars = ['Benz', 'Innova', 'Breeza', 'Etios', 'Dzire'];
+cars.splice(2, 0, 'ambassedor', 'BMW', 'Audi');
+console.log("cars :", cars);
+
+let existing = ['Benz', 'Innova', 'Breeza', 'Etios', 'Dzire'];
+let newone = existing.splice(1,2)
+console.log("existing:",existing)
+console.log("newone:",newone)
 // slice() creates a new array with a portion of elements copied from the original array, while splice() mutates the original array itself by removing, replacing or adding elements to it.
