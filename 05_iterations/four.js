7a28a716ea1a3d77bbb1bbb7cdd80136d5f9e03f
PallaviSuryawanshi0408 <128134690+PallaviSuryawanshi0408@users.noreply.github.com>
2024-10-05 16:34:02 +0530

    Update four.js

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -10,3 +10,6 @@ const Myarr =[1,2,3,4,5]
 for (const key in Myarr) {
     console.log(`${key} :-> ${Myarr[key]}`);
 }
+
+// for...in iterates over keys (or indexes for arrays) and is typically used with objects.
+// for...of iterates over values and is used with iterables like arrays, maps, and sets, but not plain objects.
